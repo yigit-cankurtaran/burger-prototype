64ea96f9e67fdb1fb9c670c27ebd3195af074a5c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-11-08 08:22:06 +0300

    deployment theme error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import BurgerRestaurantWebsite from './BurgerRestaurantWebsite.tsx'
 // i don't even know why i have this config here
-const theme = {
-  config: {
-    initialColorMode: 'dark',
-    useSystemColorMode: true
-  }
-}
+// const theme = {
+//   config: {
+//     initialColorMode: 'dark',
+//     useSystemColorMode: true
+//   }
+// }
 
 export default function App() {
   return (
