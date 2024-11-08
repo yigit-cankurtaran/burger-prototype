3b4a1235073b0b4171882ac8001058f3c7a8df20
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-11-08 11:43:58 +0300

    attempting to fix the edge request issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'esbuild',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
